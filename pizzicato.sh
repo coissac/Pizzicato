08390f8d0b95bbc4742fc6b5e8e020d647e2ac53
coissac <coissac@users.noreply.github.com>
2021-03-04 16:04:40 +0100

    Patch the way for registering the lesbonscomptes repository

--- pizzicato.sh
+++ pizzicato.sh
@@ -2,6 +2,85 @@
 
 #####################
 #
+# Options de configuration
+#
+#####################
+
+
+##
+## Paramettre system
+##
+
+HOSTNAME=pizzicato           # Nom de la machine sur le réseau local
+ACTIVER_SSH_SERVEUR=1        # Si 1 le serveur ssh d'acces distant est activé
+
+#####################
+#
+# fonctions utilitaires
+#
+#####################
+
+function change_hostname() {
+  local newname=$1
+  local tmp=$(mktemp)
+  
+  echo "$newname" > /etc/hostname
+  
+  sudo cp /etc/hosts /etc/hosts.ori.$(date '+%Y%m%d_%k%M')
+  
+  awk -v hostname="$newname" \
+      'BEGIN        {OFS="\t"} \
+       /^127.0.1.1/ {$NF=hostname} \
+                    {print $0}' /etc/hosts \
+    > "$tmp"
+    
+  mv "$tmp" /etc/hosts
+}
+
+function edit_config() {
+  local parametre=$1
+  shift
+  local value=$*
+  local tmp=$(mktemp)
+
+  awk -v date="$(date '+%d/%m/%Y at %k:%M')" \
+          '/^ *'${parametre}' *=.*$/ {print; \
+                                      print "# commented out by Jukebox.sh on",date; \
+                                      printf("# ")} \
+           {print $0}' /boot/config.txt \
+    | awk -v date="$(date '+%d/%m/%Y at %k:%M')" \
+          -v param="${parametre}" \
+          -v value="${value}" \
+          '{print $0} \
+           END {print "#"; \
+                print "# Edited on",date,"by Jukebox.sh"; \
+                print "#"; \
+                print param"="value; \
+                print }' > "${tmp}"
+                
+  mv "${tmp}" /boot/config.txt
+  chown root:root /boot/config.txt
+}
+
+
+function download_url() {
+  local url="$1"
+  local tmp=$(mktemp)
+  
+  local filename=$(basename "$url")
+  
+  if [[ "$filename" == "download" ]] ; then
+    filename=$(basename $(dirname $SQUEEZE_URL) )
+  fi
+  
+  wget -O "${filename}" "$url" 
+  
+  echo ${filename}
+}
+
+
+#####################
+#
 # Update the system with the latest version
 # of every packages
 #
@@ -16,13 +95,21 @@ echo "Updating the Raspberry PI OS system" 1>&2
 # https://www.lesbonscomptes.com/upmpdcli/pages/downloads.html#debian
 #
 
+echo "  - Register the lesbonscomptes package signature..." 1>&2
+
+tmpkeydir=$(mktemp -d /tmp/tmp.keyring.XXXXXXXX)
+gpg --no-default-keyring \
+    --homedir $tmpkeydir \
+    --keyring /usr/share/keyrings/lesbonscomptes.gpg \
+    --keyserver hkp://keyserver.ubuntu.com:80 \
+    --recv-keys F8E3347256922A8AE767605B7808CE96D38B9201
+rm -rf $tmpkeydir
+
+echo "    Done." 1>&2
+echo
+
 echo "  - Register the lesbonscomptes package repository..." 1>&2
 
-gpg --no-default-keyring \
-    --keyring /etc/apt/trusted.gpg.d/lesbonscomptes.gpg  \
-    --keyserver pool.sks-keyservers.net \
-    --recv-key F8E3347256922A8AE767605B7808CE96D38B9201
-    
 curl https://www.lesbonscomptes.com/upmpdcli/pages/upmpdcli-rbuster.list \
     > /etc/apt/sources.list.d/upmpdcli-rbuster.list
     
@@ -47,11 +134,37 @@ echo
 
 echo "  - Upgrade every packages..." 1>&2
 
-apt-get upgrade --assume-yes
+apt-get dist-upgrade --assume-yes
 
 echo "    Done." 1>&2
 echo
 
+echo "Done." 1>&2
+
+
+#####################
+#
+# Configure the system
+#
+#####################
+
+#
+# Change the hostname
+#
+
+echo "  - changing the host name from $(hostname) to ${HOSTNAME}..." 1>&2
+change_hostname "${HOSTNAME}"
+echo "    Done." 1>&2
+
+#
+# Activate the SSH server
+#
+
+if (( ACTIVER_SSH_SERVEUR == 1 )) ; then
+  echo "  - Activating the ssh server..." 1>&2
+  systemctl enable ssh
+  echo "    Done." 1>&2
+fi  
 
 echo "Done." 1>&2
 
@@ -72,6 +185,20 @@ echo "Configuring the HDMI 5'' Display" 1>&2
 #
 
 echo "  - Editing the /boot/config.txt file..." 1>&2
+
+cp /boot/config.txt /boot/config.txt.ori.$(date '+%Y%m%d_%k%M')
+
+if [ ! -z "${GPU_MEMORY}" ] ; then
+   edit_config gpu_mem "${GPU_MEMORY}"
+fi
+   
+if [ ! -z "${DECODE_MPG2}" ] ; then
+   edit_config decode_MPG2 "${GPU_MEMORY}"
+fi
+
+if [ ! -z "${DECODE_WVC1}" ] ; then
+   edit_config decode_WVC1 "${GPU_MEMORY}"
+fi  
 
 cp /boot/config.txt /boot/config.txt.pizzicato_orig
 (grep -E -v 'hdmi_(group|mode|cvt)' /boot/config.txt.pizzicato_orig \
